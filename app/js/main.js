0732ab4f9e48ee5809574dfceee7436e78611da8
shadkonstantin <shadkonstantin@yandex.ru>
2020-04-10 17:30:20 +0500

    add section trending

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
     var mixer = mixitup('.products__inner-box');
-
+    $('.trending__inner').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+    });
 
 });
